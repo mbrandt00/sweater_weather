a1ed8e6aba0a2e74dee34d8fb2e0d65db10b9017
Michael Brandt <msbrandt00@gmail.com>
2022-04-22 19:10:39 -0600

    Initialize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forecasts, only: [:show, :index]
+      resources :forecast, only: [:show, :index]
     end
   end
 end
