d5a9771808abb447698683b2c28e2e2e093972f8
Michael Brandt <msbrandt00@gmail.com>
2022-04-25 09:30:29 -0600

    Feature: clean up location string

--- app/poros/restaraunt.rb
+++ app/poros/restaraunt.rb
@@ -1,5 +1,10 @@
 class Restaraunt 
+    attr_reader :name, :address
     def initialize(data) 
-        binding.pry
+        @name = data[:name]
+        @address = cleaned_location(data[:location])
+    end
+    def cleaned_location(messy_location)
+        "#{messy_location[:address1]}, #{messy_location[:address2]}, #{messy_location[:city]}, #{messy_location[:state]} #{messy_location[:zip_code]}"
     end
 end
