42f7e747b24e37a9b3f951903ad43ef1d12d31b8
Michael Brandt <msbrandt00@gmail.com>
2022-04-25 09:51:48 -0600

    Test structure of JSON response

--- spec/requests/yelp_request_spec.rb
+++ spec/requests/yelp_request_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'yelp requests' do 
     describe 'happy path' do 
-        it 'will return food information for the destination of a roadtrip', :vcr do 
+        it 'will return the desired JSON', :vcr do 
             get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=chinese'
+            response_info = JSON.parse(response.body, symbolize_names: true)
+            expect(response_info[:data][:attributes]).to include(:destination_city, :travel_time, :forecast, :restaraunt)
+            expect(response_info[:data][:attributes][:forecast]).to include(:summary, :temperature)
+            expect(response_info[:data][:attributes][:restaraunt]).to include(:name, :address)
+            # Testing of specific values such as response_info[:data][:attributes][:forecast][:temperature].to eq(...) might yield failures due to vagaries in the weather
+            # so specific testing beyond structure of response will be omitted. 
         end
     end
 end
